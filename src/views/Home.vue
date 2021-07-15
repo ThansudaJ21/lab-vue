173e87ede6644e3c23221d1a7541e8d92fc9ce76
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-15 19:55:28 +0700

    Done 4.2
    Move to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+   <h1>Events For Good</h1>  <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <Categories v-for="catagories in events" :key="catagories.id" :event="catagories" />
