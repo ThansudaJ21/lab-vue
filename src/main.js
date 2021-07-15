ed3ab63848c5df213c62cf4d5b1b2051a4ff8edd
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-07-15 16:15:34 +0700

    Done(3.11)
    Code before starting 3.12

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-let x = 'hsnfksdflsd'
-console.log(x)
+// let x = 'hsnfksdflsd'
+// console.log(x)
 
 createApp(App).use(store).use(router).mount('#app')
